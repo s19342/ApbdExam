da92f586ca05e496d337b8442108dceabc1c6e0e
SeksiHairAndAlll <adnansazam@gmail.com>
2020-04-29 17:46:04 +0200

    initial commit

---
+++ ExamTask/ExamTask/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExamTask.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExamTask.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private readonly IDbService _dbService;
+
+        public TestController(IDbService dbService)
+        {
+            _dbService = dbService;
+        }
+    }
+}
---
+++ ExamTask/ExamTask/Services/IDbService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExamTask.Services
+{
+    public interface IDbService
+    {
+    }
+}
